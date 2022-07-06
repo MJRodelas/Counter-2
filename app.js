921c0610a04838577d923976593c0135af1237c9
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-07 06:05:01 +0800

    Adding activity when buttons were clicked
    Making buttons work
    Increase
    Decrease
    Reset

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ let count = 0;
 
 //select value and buttons
 const value = document.querySelector('#value')
-const btns =document.querySelectorAll(".btn")
+const btns = document.querySelectorAll(".btn")
 
 console.log(btns)
 
 btns.forEach(function(btn){
     btn.addEventListener('click',function(e){
         const styles = e.currentTarget.classList
+
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
     })
 });
